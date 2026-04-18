eb9d229d472a29c76df685ac9ff7350ce5ca2cb4
Josue <102492225+JosueMT505@users.noreply.github.com>
2026-04-17 19:10:26 -0500

    BaseEntity modificado

--- src/main/java/com/UrbanCheck/UrbanCheck_app/model/entity/BaseEntity.java
+++ src/main/java/com/UrbanCheck/UrbanCheck_app/model/entity/BaseEntity.java
@@ -2,11 +2,29 @@ package com.UrbanCheck.UrbanCheck_app.model.entity;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PreUpdate;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     
+    @Column(name = "creado_en", updatable = false)
+    private LocalDateTime creadoEn;
+
+    @Column(name = "actualizado_en")
+    private LocalDateTime actualizadoEn;
+
+    @PrePersist
+    protected void onCreate() {
+        creadoEn = LocalDateTime.now();
+        actualizadoEn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        actualizadoEn = LocalDateTime.now();
+    }
 }
